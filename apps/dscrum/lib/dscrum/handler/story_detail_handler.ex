e581f5addea4f4713201888a2823602b9c19868a
ruben.baeza <ruben.baeza@santiago.mx>
2019-05-27 08:36:00 -0500

    ajuste de usuarios online en push de detalle

--- apps/dscrum/lib/dscrum/handler/story_detail_handler.ex
+++ apps/dscrum/lib/dscrum/handler/story_detail_handler.ex
@@ -8,13 +8,15 @@ defmodule Dscrum.StoryDetailHandler do
     StoryDetailStruct,
     StoryUserStruct
   }
+  alias DscrumWeb.Presence
+
 
   def index(socket) do
     users = StoryQuery.detail_story_user(socket.assigns.story_id, socket.assigns.guardian_default_resource.id)
       |> Repo.all
       |> Enum.map(fn(res) ->
           StoryUserStruct.new(res)
-            |> last_difficulty(socket.assigns.story_id)
+            |> last_difficulty(socket.assigns.story_id, socket)
         end)
     socket.assigns.story_id
       |> StoryQuery.detail
@@ -23,14 +25,19 @@ defmodule Dscrum.StoryDetailHandler do
       |> Map.put(:users, users)
   end
 
-  def last_difficulty(%StoryUserStruct{} = command, story_id) do
+  def last_difficulty(%StoryUserStruct{} = command, story_id, socket) do
     difficulty = StoryIterationQuery.last_iteration_story(command.id, story_id)
       |> Repo.one
-
-    command
-      |> Map.put(:online, false)
-      |> Map.put(:difficulty_name, difficulty.difficulty_name)
-      |> Map.put(:difficulty_id, difficulty.difficulty_id)
+    case Presence.list(socket)["user_story:" <> Integer.to_string(command.id)] do
+      nil -> command
+            |> Map.put(:online, false)
+            |> Map.put(:difficulty_name, difficulty.difficulty_name)
+            |> Map.put(:difficulty_id, difficulty.difficulty_id)
+      _ -> command
+            |> Map.put(:online, true)
+            |> Map.put(:difficulty_name, difficulty.difficulty_name)
+            |> Map.put(:difficulty_id, difficulty.difficulty_id)
+    end
 
   end
 end
